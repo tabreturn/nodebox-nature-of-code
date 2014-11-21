0eed7983f9d3b73155cfa01dc8364c83f7ffc201
tristan <github@hxgraphics.com>
2014-11-21 20:05:55 +0200

    - replace example 1.10

--- 01-vectors/01.01.py
+++ 01-vectors/01.01.py
@@ -1,35 +1,28 @@
 # Example 1.10: Accelerating towards the mouse
 
-class Walker:
-    def __init__(self):
-        self.x = WIDTH/2
-        self.y = HEIGHT/2
-    
-    def step(self):
-        r = random(1.0)
-        if(r < 0.4):
-            self.x += 1
-        elif(r < 0.6):
-            self.x -= 1
-        elif(r < 0.8):
-            self.y += 1
-        else:
-            self.y -= 1
-    
-    def display(self):
-        nostroke()
-        rect(self.x, self.y, 1, 1)
-
-
 speed(60)
 size(640, 360)
+background(1)
 
 def setup():
-    global w 
-    w = Walker()
+    global x, y, xspeed, yspeed
+    x = 100
+    y = 100
+    xspeed = 1
+    yspeed = 3.3
 
 def draw():
-    w.step()
-    w.display()
+    global x, y, xspeed, yspeed
+    x = x + xspeed
+    y = y + yspeed
+    
+    if((x > WIDTH) or (x < 0)):
+        xspeed = xspeed * -1
+    if((y > HEIGHT) or (y < 0)):
+        yspeed = yspeed * -1
+    
+    stroke(0)
+    fill(0.6)
+    oval(x,y,16,16)
 
 
